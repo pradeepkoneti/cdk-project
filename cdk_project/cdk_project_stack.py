5b016585485a0a491af889df2c61765da8b42afd
Pradeep <skoneti@unomaha.edu>
2020-09-21 18:36:27 -0500

    Adding s3 bucket

--- cdk_project/cdk_project_stack.py
+++ cdk_project/cdk_project_stack.py
@@ -1,4 +1,8 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+
+)
 
 
 class CdkProjectStack(core.Stack):
@@ -7,3 +11,13 @@ class CdkProjectStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="cdk-project6676",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+
+            
+
+        )
